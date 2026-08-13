f035acc3dbde2794c873290843902100981e4665
Yılmaz Yaşyerli <yilmaz@pop-os.hgw.local>
2026-08-13 18:57:51 +0300

    update: Stage 2 completed.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 A probabilistic football prediction engine built from scratch.
 
 ## Progress Log
-- [x] Stage 1: Repo Setup & Python Basics('src/analyzer.py')
-- [ ] Stage 2: Pandas & DataFrames ('src/pandas_analyzer.py')
+- [x] Stage 1: Repo Setup & Python Basics ('src/analyzer.py')
+- [x] Stage 2: Pandas & DataFrames ('src/pandas_analyzer.py')
+- [ ] Stage 3: Feature Engineering & Form Analysis ('src/feature_engineering.py')
---
+++ src/pandas_analyzer.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+def match_count(df):
+    return len(df)
+
+def total_goals(df):
+    return (df['home_goals']+df['away_goals']).sum()
+
+def avg_goals(df):
+    if match_count(df) == 0:
+        return 0
+    else:
+        return total_goals(df)/match_count(df)
+
+def calculate_points(df):
+    home_pts = pd.Series(0,index=df.index)
+    away_pts = pd.Series(0,index=df.index)
+
+    home_win = df['home_goals'] > df['away_goals']
+    away_win = df['home_goals'] < df['away_goals']
+    draw = df['home_goals'] == df['away_goals']
+    
+    home_pts[home_win] = 3
+    home_pts[draw] = 1
+    home_pts[away_win] = 0
+    away_pts[home_win] = 0
+    away_pts[draw] = 1
+    away_pts[away_win] = 3
+
+    df1 = pd.DataFrame({'team':df['home_team'],'pts':home_pts})
+    df2 = pd.DataFrame({'team':df['away_team'],'pts':away_pts})
+
+    df3 = pd.concat([df1,df2]).groupby('team')['pts'].sum().sort_values(ascending=False)
+
+    print(df3)
+
+def print_summary(df):
+    print(match_count(df))
+    print(total_goals(df))
+    print(avg_goals(df))
+
+
+if __name__ == '__main__':
+    df = pd.read_csv("../data/matches.csv")
+    print_summary(df)
+    calculate_points(df)
+    
